bec139c93b7b4a313a528359939007e5f6fa78ae
nagamani <nagamani.ngowda@gmail.com>
2024-03-07 19:30:25 +0530

    pushing to docker hub

---
+++ vars/DockerImagePush.groovy
@@ -0,0 +1,16 @@
+ def call(String project, String ImageTag, String hubUser){
+    withCredentials([usernamePassword(
+       credentialsId: 'docker_credentials', 
+       passwordVariable: 'PASS', 
+       usernameVariable: 'USER')])
+       )]) {
+
+         sh "docker login -u '$USER' -p '$PASS'"
+        }
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"   
+ }
+
+
+
+    
