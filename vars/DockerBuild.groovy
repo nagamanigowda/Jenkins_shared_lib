58500a21966ab52d5fd9faa3abd0f28dfbe5be9d
nagamani <nagamani.ngowda@gmail.com>
2024-03-07 17:29:56 +0530

    docker build succeeded

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,12 +1,10 @@
 def call(String project, String ImageTag, String hubUser){
     sh """
-      docker image build -t ${hubUser}/${project}
+      docker image build -t ${hubUser}/${project} .
       Docker image tag ${hubUser}/${project}  ${hubUser}/${project}:${ImageTag}
       Docker image tag ${hubUser}/${project}  ${hubUser}/${project}:latest
       """
    
-
-
 }
 
 
