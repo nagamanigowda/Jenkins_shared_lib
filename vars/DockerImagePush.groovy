8394d8707e3e0c19a8032eac6d515112eb55839c
nagamani <nagamani.ngowda@gmail.com>
2024-03-08 12:38:04 +0530

    docker push update

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -1,15 +1,15 @@
-//  def call(String project, String ImageTag, String hubUser){
-//     withCredentials([usernamePassword(
-//        credentialsId: 'docker_credentials', 
-//        passwordVariable: 'PASS', 
-//        usernameVariable: 'USER')])
-//        )]) {
+ def call(String project, String ImageTag, String hubUser){
+     withCredentials([usernamePassword(
+        credentialsId: 'docker_credentials', 
+        passwordVariable: 'PASS', 
+        usernameVariable: 'USER')])
+        )]) {
 
-//          sh "docker login -u '$USER' -p '$PASS'"
-//         }
-//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-//     sh "docker image push ${hubUser}/${project}:latest"   
-//  }
+          sh "docker login -u '$USER' -p '$PASS'"
+         }
+     sh "docker image push ${hubUser}/${project}:${ImageTag}"
+     sh "docker image push ${hubUser}/${project}:latest"   
+  }
 
 
 
