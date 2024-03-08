9abf457e583a2bdec1e3ef7829a92dbf4bb06ce3
nagamani <nagamani.ngowda@gmail.com>
2024-03-08 14:06:41 +0530

    docker images pushed to ECR

--- vars/DockerBuild.groovy
+++ vars/DockerBuild.groovy
@@ -1,12 +1,19 @@
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
+    
+//     sh """
+//      docker image build -t ${hubUser}/${project} . 
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     
+//     """
+//  }
+
+def call(String aws_account_id, String region, String ecr_repo_name){
     
     sh """
-     docker image build -t ${hubUser}/${project} . 
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-     
-    """
- }
+     docker build -t ${ecr_repo_name} .
+     docker tag ${ecr_repo_name} :latest ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name} :latest
 
+     """
 
 
