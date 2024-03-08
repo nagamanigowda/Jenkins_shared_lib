2d2ed9f13cb7d2ac43cdaf7f61ec4a5325f62e2b
nagamani <nagamani.ngowda@gmail.com>
2024-03-08 18:37:38 +0530

    docker push file modified

--- vars/DockerImagePush.groovy
+++ vars/DockerImagePush.groovy
@@ -16,8 +16,7 @@ def call(String aws_account_id, String region, String ecr_repo_name){
     
     sh """
 
-     def awsPassword = sh(script: 'aws ecr get-login-password --region ${region}', returnStdout: true).trim()
-     sh "echo '$awsPassword' | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com"
+     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
      docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repo_name}:latest
 
      """
